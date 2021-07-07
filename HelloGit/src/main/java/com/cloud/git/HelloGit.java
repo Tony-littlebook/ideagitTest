67dc8554f1a8ba68b7261810745e660ee33b8154
Tony-littlebook <1287625911@qq.com>
2021-07-07 16:59:04 +0800

    创建分支

--- HelloGit/src/main/java/com/cloud/git/HelloGit.java
+++ HelloGit/src/main/java/com/cloud/git/HelloGit.java
@@ -4,6 +4,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello Git!");
         System.out.println("update firstly");
-        System.out.println("update secondly");
+        System.out.println("create branches");
     }
 }
